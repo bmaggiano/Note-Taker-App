2aba7fcc84c0bd34d4d7a7ff384af5f3abab036f
bmaggiano <brandon.maggiano@gmail.com>
2022-10-17 16:30:31 -0700

    fixed PORT issue

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const notes = require('./db/db.json');
 
 const app = express();
-const port = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
